fc92ae8b4cb7dcb6ccad860b4350a9748ec5b474
Riya Sheik <rsheik@berkeley.edu>
2023-11-28 07:09:26 +0000

    more lint fixes

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -2,9 +2,10 @@
 
 require 'rails_helper'
 require 'google/apis/civicinfo_v2'
-describe Representative do 
+describe Representative do
   subject(:representatives) { described_class.civic_api_to_representative_params(result) }
-  before do 
+
+  before do
     url =
       'https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=4020%20Ashby%20Ln%20Indian%20Land%20SC%2029707&key=google-api-key'
     json = File.read(Rails.root.join('fixtures/representatives.json'))
@@ -20,7 +21,7 @@ describe Representative do
   let(:result) { service.representative_info_by_address(address: address) }
 
   describe '#civic_api_to_representative_params' do
-    before { described_class.create!({name: 'Joseph R. Biden'}) }
+    before { described_class.create!({ name: 'Joseph R. Biden' }) }
 
     it 'does not add a duplicate representative to reps list' do
       expect { representatives }.to change(described_class, :count).by(20)
